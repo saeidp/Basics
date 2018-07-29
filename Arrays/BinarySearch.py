c9b5c56fd68d3d7966ddc177cfc9d158f33306d7
saeid <saeidpahlevan@hotmail.com>
2018-07-29 18:48:15 +0800

    Binary Search

---
+++ Arrays/BinarySearch.py
@@ -0,0 +1,12 @@
+def binarySearch(array, targetValue):
+  min = 0
+  max = len(array) -1
+  while(min <= max):
+    mid = min + (max - min) // 2
+    if(array[mid] == targetValue):
+      return mid
+    elif(targetValue < array[mid]):
+      max = mid - 1
+    else:
+      min = mid -1
+  return -1 
