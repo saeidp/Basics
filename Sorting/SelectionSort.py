8804cd94e9d459055f3114cc7392a8c8d0e62779
saeid <saeidpahlevan@hotmail.com>
2018-09-18 07:01:39 +0800

    Sorting: simplified Selection Sort

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -1,22 +1,20 @@
+# First, find the smallest item in the array, 
+# and exchange it with the first entry. Then, find the
+# next smallest item and exchange it with the second
+# entry. Continue in this way until the entire array is
+# sorted. This method is called selection sort because it
+# works by repeatedly selecting the smallest remaining 
+# item (N-1) + (N-2) + …1+ 0 
+
 def selectionSort(array):
     n = len(array)
-    for i in range(n):
-        minIndex = indexOfMinimum(array, i)
-        swap(array, i, minIndex)
-    return array
-
-def swap(array, first_index, second_index):
-    array[first_index], array[second_index] = array[second_index], array[first_index]
-    return array
-  
-def indexOfMinimum(array, startIndex):
-    minValue = array[startIndex]
-    minIndex = startIndex
-    for i in range(startIndex + 1,len(array)):
-        if(array[i] < minValue):
-            minValue = array[i]
-            minIndex = i
-    return minIndex
+    for i in range(n - 1):
+        min = i
+        for j in range(i + 1 , n):
+            if array[j] < array[min]:
+                min = j
+        array[i], array[min] = array[min], array[i]
 
 array = [10,12,5,11,3]
-print(selectionSort(array))
+selectionSort(array)
+print(array)
