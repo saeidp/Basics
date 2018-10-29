4eb642b3f2954c6c4ac328daaaecb50b7f5a001b
saeidpahlevan@hotmail.com <s.pahlevan@fugro.com>
2018-10-29 13:33:47 +0800

    Queue:  Reversing First k Elements of Queue

---
+++ Stack and Queue/QueueReverseFirstKElements.py
@@ -0,0 +1,80 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+class Queue:
+
+    def __init__(self):
+        self.items = []
+    
+    def enqueue(self, item):
+        self.items.insert(0, item)
+    
+    def dequeue(self):
+       return self.items.pop()
+    
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+
+def reverseFirstK(queue, k):
+    if queue.isEmpty == False:
+        return
+    if k > queue.size():
+        return
+    stack= Stack()
+    queueReverse = Queue()
+    for _ in range(k):
+        stack.push(queue.dequeue())
+    
+    for _ in range(stack.size()):
+        queueReverse.enqueue(stack.pop())
+    
+    for _ in range(queue.size()):
+        queueReverse.enqueue(queue.dequeue())
+    return queueReverse
+
+
+# k=5
+# queue: 1 2 3 4 5 6 7 8 9 10
+# queueNew: 5 4 3 2 1 6 7 8 9 10
+queue = Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+queue.enqueue(8)
+queue.enqueue(9)
+queue.enqueue(10)
+
+queueNew = reverseFirstK(queue, 5)
+print(queueNew.dequeue(), end=' ')
+print(queueNew.dequeue(), end=' ')
+print(queueNew.dequeue(), end=' ')
+print(queueNew.dequeue(), end=' ')
+print(queueNew.dequeue(), end=' ')
+print(queueNew.dequeue(), end=' ')
+print(queueNew.dequeue(), end=' ')
+print(queueNew.dequeue(), end=' ')
+print(queueNew.dequeue(), end=' ')
+print(queueNew.dequeue())
