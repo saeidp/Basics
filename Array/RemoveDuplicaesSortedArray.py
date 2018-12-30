5424c7fdea1098fb53bf965ae1781d92dbf4e4bd
Saeid <saeidpahlevan@hotmail.com>
2018-12-31 06:44:46 +0800

    Array: Remove duplicates in sorted array

--- Array/RemoveDuplicaesSortedArray.py
+++ Array/RemoveDuplicaesSortedArray.py
@@ -4,16 +4,34 @@
 # Your function should return length = 2. It doesn't matter
 #  what you leave beyond the new length
 # o(n) and space o(1)
-def removeDuplicate(nums):
+def removeDuplicates(nums):
     if len(nums) == 0:
         return 0
+    j = 0
+    for i in range(1,len(nums)):
+        if nums[i] != nums[j]:
+            j += 1
+            nums[j] = nums[i]
+    return j + 1
+
+
+print(removeDuplicates([0,1,2,2,3])) # 4
+
+#-------------------------------------
+def removeDuplicates2(nums):
+    if len(nums) == 0:
+        return 0
+    n = len(nums) - 1
     i = 0
-    for j in range(1,len(nums)):
-        if nums[j] != nums[i]:
-            i += 1
-            nums[i] = nums[j]
-    return i + 1
+    j = 1
+    
+    while i < n:
+        if nums[i] < nums[i + 1]:
+            nums[j] = nums[i+1]
+            j += 1
+        i += 1
+    return j
+print(removeDuplicates2([0,1,2,2,3])) # 4
+print(removeDuplicates2([1,1,2])) # 2
 
-
-print(removeDuplicate([0,1,2,2,3])) // 4
 
