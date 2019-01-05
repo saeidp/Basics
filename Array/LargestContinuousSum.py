88cf724d83da56bbc9d2188288f6fa8758858deb
Saeid <saeidpahlevan@hotmail.com>
2019-01-05 08:06:43 +0800

    Array: Largest Continuous Sum

--- Array/LargestContinuousSum.py
+++ Array/LargestContinuousSum.py
@@ -1,5 +1,24 @@
 # Given an array of integers (positive and negative) find the largest
 # continuous sum.
+def find_max_sum_sub_array(A):
+  if len(A) < 1:
+    return 0
+
+  curr_max = A[0]
+  global_max = A[0]
+  lengthA = len(A)
+  for i in range(1, lengthA):
+    if curr_max < 0:
+      curr_max = A[i]
+    else:
+      curr_max += A[i]
+
+    if global_max < curr_max:
+      global_max = curr_max
+
+  return global_max
+print(find_max_sum_sub_array([1,2,-1,3,4,10,10,-10,-1])) #29
+#------------------------------------------------
 def large_cont_sum(arr):
     if len(arr)==0:   # Check to see if array is length 0
         return 0
@@ -18,3 +37,5 @@ def large_cont_sum(arr):
     return max_sum
 
 print(large_cont_sum([1,2,-1,3,4,10,10,-10,-1])) #29
+
+
