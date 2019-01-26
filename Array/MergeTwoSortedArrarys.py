126a4f24c693130509ab851cce4dda34813c962f
Saeid <saeidpahlevan@hotmail.com>
2019-01-27 07:57:05 +0800

    Array: Merge two sorted arrays

--- Array/MergeTwoSortedArrarys.py
+++ Array/MergeTwoSortedArrarys.py
@@ -1,28 +1,30 @@
-# Merge two arrays in order
+# Merge two sorted arrays in order
 # MergeArrays([1,3,4,5] , [2,6,7,8]) -> [1,2,3,4,5,6,7,8]
 def mergeArrays(arr1, arr2):
   i = 0
   j = 0
   k = 0
-  arr = []
   n1 = len(arr1)
   n2 = len(arr2)
-
+  arr = [-1] * (n1 + n2)
+  
   while(i < n1 and j <  n2):
     if(arr1[i] < arr2[j]):
-      arr.append(arr1[i])
+      arr[k] = arr1[i]
       i += 1
-      k += 1
     else:
-      arr.append(arr2[j])
+      arr[k] = arr2[j]
       j +=1
-      k += 1
+    k +=1
+
   while(i < n1 ):
-    arr.append(arr1[i])
+    arr[k] = arr1[i]
     i += 1
+    k +=1
   while(j <n2):
-    arr.append(arr2[j])
+    arr[k] = arr2[j]
     j += 1
+    k +=1
   return arr
 
 arr = mergeArrays([1,3,4,5] , [2,6,7,8])
