488129b5f098b4b3ac7529fa6fcba6d718bd515c
Saeid Pahlevan <s.pahlevan@fugro.com>
2019-02-15 09:51:42 +0800

    LinkedList- Refactored to use head pointer instead full linked list

--- Linked List/Singly/Reverse.py
+++ Linked List/Singly/Reverse.py
@@ -1,84 +1,72 @@
-# Linked List head is Node(-1)
 class Node:
     def __init__(self, data):
         self.data = data
-        self.nextElement = None
+        self.next = None
 
-
-class LinkedList:
-    def __init__(self):
-        self.headNode = Node(-1)
-
-    def isEmpty(self):
-        if(self.headNode.nextElement == None):
-            return True
-        else:
-            return False
-
-    def getHead(self):
-        return self.headNode
-
-    def printList(self):
-        if(self.isEmpty()):
-            print("List is Empty")
-            return False
-        temp = self.headNode.nextElement
-        while(temp.nextElement != None):
-            print(temp.data, "->", end=" ")
-            temp = temp.nextElement
-        print(temp.data, "-> None")
-        return True
-
-    def insertAtHead(self, data):
-        node = Node(data)
-        node.nextElement = self.headNode.nextElement
-        self.headNode.nextElement = node
-        return self.headNode
-
-
-
-def reverse2(list):
-    if(list.isEmpty()):
-        return LinkedList()
-    list2 = LinkedList()
-    current = list.getHead().nextElement
+#input: 0->1->2->3->4->5->None
+#output: 6->5->4->3->2->1->None
+def reverse2(head):
+    if head == None:
+        return None
+    head1 = Node(head.data)
+    current = head.next
     while(current != None):
-        list2.insertAtHead(current.data)
-        current = current.nextElement
-    return list2
+        node = Node(current.data)
+        node.next = head1
+        head1 = node
+        current = current.next
+    return head1
 
-# list = LinkedList()
-# list.insertAtHead(6)
-# list.insertAtHead(4)
-# list.insertAtHead(9)
-# list.insertAtHead(10)
-# list.printList()
+node0 = Node(0)
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
 
-# list2 = reverse2(list)
-# list2.printList()
+node0.next = node1
+node1.next =  node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = None 
+
+temp = reverse2(node0)
+while(temp.next != None):
+    print(temp.data, "->", end=" ")
+    temp = temp.next
+print(temp.data, "-> None")
 #---------------------------------------------------------
-
-def reverse(list):
+def reverse(head):
   prev = None #To keep track of the previous element, will be used in swapping links
-  current = list.getHead().nextElement #firstElement
+  current = head #firstElement
   next = None
-  
   #While Traversing the list, swap links
   while (current != None):
-    next = current.nextElement
-    current.nextElement = prev
+    next = current.next
+    current.next = prev
     prev = current
     current = next
-	
   #Linking Head Node with the new First Element
-  list.headNode.nextElement = prev
+  head = prev
+  return head
 
-list = LinkedList()
-list.insertAtHead(6)
-list.insertAtHead(4)
-list.insertAtHead(9)
-list.insertAtHead(10)
-list.printList()
+node0 = Node(0)
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
 
-reverse(list)
-list.printList()
+node0.next = node1
+node1.next =  node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = None 
+
+temp = reverse(node0)
+while(temp.next != None):
+    print(temp.data, "->", end=" ")
+    temp = temp.next
+print(temp.data, "-> None")
