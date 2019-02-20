eeb33d58e2dbbd36f65f80a4b8da87fb400041b8
Saeid Pahlevan <s.pahlevan@fugro.com>
2019-02-20 09:22:20 +0800

    Queue: Reverse first k elements of a queue

--- Stack and Queue/QueueReverseFirstKElements.py
+++ Stack and Queue/QueueReverseFirstKElements.py
@@ -33,7 +33,11 @@ class Queue:
     def size(self):
         return len(self.items)
 
-
+# k=5
+# queue: 1 2 3 4 5 6 7 8 9 10
+# queueNew: 1 2 3 4 5 10 9 8 7 6
+# first means the values entered the first. In this sample the first 5 elements of queue
+# 6 7 8 9 10 (10 is thefirst and goes to fith)
 def reverseFirstK(queue, k):
     if queue.isEmpty == False:
         return
@@ -52,29 +56,18 @@ def reverseFirstK(queue, k):
     return queueReverse
 
 
-# k=5
-# queue: 1 2 3 4 5 6 7 8 9 10
-# queueNew: 5 4 3 2 1 6 7 8 9 10
-queue = Queue()
-queue.enqueue(1)
-queue.enqueue(2)
-queue.enqueue(3)
-queue.enqueue(4)
-queue.enqueue(5)
-queue.enqueue(6)
-queue.enqueue(7)
-queue.enqueue(8)
-queue.enqueue(9)
-queue.enqueue(10)
 
-queueNew = reverseFirstK(queue, 5)
-print(queueNew.dequeue(), end=' ')
-print(queueNew.dequeue(), end=' ')
-print(queueNew.dequeue(), end=' ')
-print(queueNew.dequeue(), end=' ')
-print(queueNew.dequeue(), end=' ')
-print(queueNew.dequeue(), end=' ')
-print(queueNew.dequeue(), end=' ')
-print(queueNew.dequeue(), end=' ')
-print(queueNew.dequeue(), end=' ')
-print(queueNew.dequeue())
+queue = Queue()
+queue.enqueue(10)
+queue.enqueue(9)
+queue.enqueue(8)
+queue.enqueue(7)
+queue.enqueue(6)
+queue.enqueue(5)
+queue.enqueue(4)
+queue.enqueue(3)
+queue.enqueue(2)
+queue.enqueue(1)
+
+queueNew = reverseFirstK(queue, 4)
+print ("done")
